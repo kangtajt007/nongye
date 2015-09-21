9bd1077b99c02b4be1dfdbda0bb317722bbe44a1
heyifan <kangtajt007@163.com>
2015-09-21 14:24:45 +0800

    完成农业气象部分功能

--- js/util.js
+++ js/util.js
@@ -2,126 +2,88 @@
  * 等值线图设置
  */
 function contourLine(){
-        BootstrapDialog.show({
-            title: "等值线图设置",
-            url:'contourLine.html',
-            closable:false,
-            size:'size-wide',
-            buttons: [{
-                id: 'btn-ok',
-                label: '确定',
-                cssClass: 'btn btn-sm btn-corner btn-info fa fa-save',
-                autospin: false,
-                action: function(dialogRef){
-                    outputContourLineImage();
-                    dialogRef.close();
-                }
-            },
-                {
-                    id: 'btn-cancel',
-                    label: '取消',
-                    cssClass: 'btn btn-sm btn-corner btn-default fa fa-undo',
-                    autospin: false,
-                    action: function(dialogRef){
-                        dialogRef.close();
-                    }
-                }]
-        });
-    }
-/**
- * 等值线图
- */
-    function outputContourLineImage(){
-        BootstrapDialog.show({
-            title: "等值线图",
-            url:'contourLineImage.html',
-            closable:false,
-            size:'size-wide',
-            buttons: [{
-                id: 'btn-ok',
-                label: '保存',
-                cssClass: 'btn btn-sm btn-corner btn-info fa fa-save',
-                autospin: false,
-                action: function(dialogRef){
-                    dialogRef.close();
-                }
-            },
-                {
-                    id: 'btn-cancel',
-                    label: '关闭',
-                    cssClass: 'btn btn-sm btn-corner btn-default fa fa-undo',
-                    autospin: false,
-                    action: function(dialogRef){
-                        dialogRef.close();
-                    }
-                }]
-        });
-    }
-    
-    function siteSelect (){
-    	BootstrapDialog.show({
-        title: "站点选择",
-        message: $("#siteSelect-container").clone().removeClass("hidden").show(),
+    BootstrapDialog.show({
+        title: "等值线图设置",
+        url:'contourLine.html',
         closable:false,
-        height:'425',
+        width:'740',
+        buttons: [{
+            id: 'btn-ok',
+            label: '确定',
+            cssClass: 'btn btn-sm btn-corner btn-info fa fa-save',
+            autospin: false,
+            action: function(dialogRef){
+                outputContourLineImage();
+                dialogRef.close();
+            }
+        },
+            {
+                id: 'btn-cancel',
+                label: '取消',
+                cssClass: 'btn btn-sm btn-corner btn-default fa fa-undo',
+                autospin: false,
+                action: function(dialogRef){
+                    dialogRef.close();
+                }
+            }]
+    });
+}
+/**
+* 等值线图
+*/
+function outputContourLineImage(){
+    BootstrapDialog.show({
+        title: "等值线图",
+        url:'contourLineImage.html',
+        closable:false,
         size:'size-wide',
         buttons: [{
             id: 'btn-ok',
-            label: '全选',
-            cssClass: 'btn btn-sm btn-corner btn-info fa fa-check-square',
-            autospin: false,
-            action: function(dialogRef){
-            	$('[name="switch-field-siteSelect"]').each(function () {
-            		if(!$(this).is(":disabled")){
-            			$(this).prop("checked", true);
-            		}
-            	});
-              
-            }
-        },{
-            id: 'btn-ok',
-            label: '清空',
-            cssClass: 'btn btn-sm btn-corner btn-info fa fa-times',
-            autospin: false,
-            action: function(dialogRef){
-            	$('[name="switch-field-siteSelect"]').each(function () {
-            		if(!$(this).is(":disabled")){
-            			$(this).prop("checked", false);
-            		}
-            	});
-            	
-            }
-        },{
-            id: 'btn-ok',
-            label: '反选',
-            cssClass: 'btn btn-sm btn-corner btn-info fa fa-check-square-o',
-            autospin: false,
-            action: function(dialogRef){
-            	  $('[name="switch-field-siteSelect"]').each(function () { 
-            			if(!$(this).is(":disabled")){
-                      $(this).prop("checked", !$(this).prop("checked"));  
-                      }
-                  });   
-            }
-        },
-        {
-            id: 'btn-ok',
-            label: '确定',
-            cssClass: 'btn btn-sm btn-corner btn-warning fa fa-save',
-            autospin: false,
-            action: function(dialogRef){
-
-                dialogRef.close();
-            }
-        },
-        {
-            id: 'btn-cancel',
-            label: '取消',
-            cssClass: 'btn btn-sm btn-corner btn-default fa fa-undo',
+            label: '保存',
+            cssClass: 'btn btn-sm btn-corner btn-info fa fa-save',
             autospin: false,
             action: function(dialogRef){
                 dialogRef.close();
             }
-        }]
+        },
+            {
+                id: 'btn-cancel',
+                label: '关闭',
+                cssClass: 'btn btn-sm btn-corner btn-default fa fa-undo',
+                autospin: false,
+                action: function(dialogRef){
+                    dialogRef.close();
+                }
+            }]
     });
-    }
+}
+/**
+ * 站点选择
+ */
+function siteSelect (){
+    BootstrapDialog.show({
+    title: "站点选择",
+    url: 'siteSelect.html',
+    closable:false,
+    height:'320',
+    buttons: [
+    {
+        id: 'btn-ok',
+        label: '确定',
+        cssClass: 'btn btn-sm btn-corner btn-info fa fa-save',
+        autospin: false,
+        action: function(dialogRef){
+            dialogRef.close();
+        }
+    },
+    {
+        id: 'btn-cancel',
+        label: '取消',
+        cssClass: 'btn btn-sm btn-corner btn-default fa fa-undo',
+        autospin: false,
+        action: function(dialogRef){
+            dialogRef.close();
+        }
+    }]
+});
+}
